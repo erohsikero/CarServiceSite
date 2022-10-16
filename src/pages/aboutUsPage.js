07b5753b789ff6053aed348353f3799cbda151a7
kishore-10664 <kishore.st@zohocorp.com>
2022-10-16 23:06:40 +0530

    About Us updated

--- src/pages/aboutUsPage.js
+++ src/pages/aboutUsPage.js
@@ -1,13 +1,13 @@
 import AchievementBanner from "../components/achievementBanner/achievementBanner";
 import Banner from "../components/Banner/banner"
 import BookAnAppointmentForm from "../components/BookAnAppointmentForm/bookAnAppointmentForm";
-import AboutUsContainer from "../components/HomePage/AboutUsContainer/aboutUsContainer";
+import AboutUsContainerFull from "../components/HomePage/AboutUsContainer/aboutUsContainerFull";
 
 const AboutUsPage = () => {
     return (
         <>
             <Banner activePageName={"About Us"}/>
-            <AboutUsContainer showButton={false}/>
+            <AboutUsContainerFull showButton={false}/>
             <AchievementBanner />
             <BookAnAppointmentForm />
         </>
